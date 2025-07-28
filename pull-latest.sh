90d6f52b52e2df467484eae81a614dfc29c2bf46
Zeff Svoboda <zeff@alteredconstants.com>
2025-07-28 17:53:30 -0500

    Move build tools to non-dev deps.

--- pull-latest.sh
+++ pull-latest.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export NODE_ENV=production
 export PORT=8001
-git pull && npm ci --include=dev && npm run build && pm2 restart alteredconstants.com
+git pull && npm ci && npm run build && pm2 restart alteredconstants.com
